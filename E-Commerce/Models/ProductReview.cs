01705394c7a1fdba513dcc214487360175de7069
Sara-Rabea <sarahfakary13@gmail.com>
2026-01-30 13:47:22 +0200

    Initial review feature scaffold

--- E-Commerce/Models/ProductReview.cs
+++ E-Commerce/Models/ProductReview.cs
@@ -13,7 +13,7 @@ namespace E_Commerce.Models
         public int ProductId { get; set; }
         public Product Product { get; set; }
 
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
         public ApplicationUser User { get; set; }
     }
 }
